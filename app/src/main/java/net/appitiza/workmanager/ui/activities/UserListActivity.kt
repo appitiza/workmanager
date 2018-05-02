443dfcfad27fe1a97a6842669b2c77cb46c786da
appitiza <appitiza@gmail.com>
2018-05-02 22:22:21 +0530

    activity added

---
+++ app/src/main/java/net/appitiza/workmanager/ui/activities/UserListActivity.kt
@@ -0,0 +1,13 @@
+package net.appitiza.workmanager.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.appitiza.workmanager.R
+
+class UserListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+    }
+}
